92eda741c52f9d693789c0c7575c0b760fdd9134
Hanksito <barjabalseiro@gmail.com>
2022-07-08 00:43:11 +0000

    listo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -7,6 +7,20 @@ import rigoImage from "../../img/rigo-baby.jpg";
 const Home = () => {
 	
 	const[history,setHistory]=useState([])
+	const deleteItem =()=>{
+		let borrar = document.querySelectorAll(".btn-close");
+		
+		for (let x of borrar) {
+		  x.addEventListener("click", function (e) { 
+			let item = e.target.parentElement; 
+		   
+			let item2 = item.parentElement;  
+			
+			//child[5].removeChild(item2);
+			item2.style.display = "none"; })
+			 }
+	  
+	  }
 	return (
 		<div className="container text-center">
 			<h1 className="fw-lighter">Todos</h1>
@@ -17,7 +31,7 @@ const Home = () => {
 				}
 			}/>
 			<ul class="list-group">
-				{ history.map( h => {return<li class="list-group-item"> {h} <span> <i class="fa fa-trash" onClick={(e) => { removeItem(e.target.value)}}> </i> </span> </li> })}
+				{ history.map( h => {return<li class="list-group-item" ><button type="button" class="btn-close" aria-label="Close" onClick={deleteItem}></button> {h}</li> })}
 				
 			</ul>
 
