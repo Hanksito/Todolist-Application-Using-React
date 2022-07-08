2da99879aafd2261a0a0c8c732f2b298f7aaf510
Hanksito <barjabalseiro@gmail.com>
2022-07-08 07:21:04 +0000

    listo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -7,17 +7,9 @@ import rigoImage from "../../img/rigo-baby.jpg";
 const Home = () => {
 	
 	const[history,setHistory]=useState([])
-	const deleteItem =()=>{
-		let borrar = document.querySelectorAll(".fa-trash");
-		for (let x of borrar) {
-		  x.addEventListener("click", function (e) { 
-			let item = e.target.parentElement; 
-			let item2 = item.parentElement;  
-			item2.style.display = "none";})
-			 }
-	  }
+
 	return (
-		<div className="container text-center">
+		<div className="container text-center shadow p-3 mb-5 bg-body rounded rounded-bottom">
 			<h1 className="fw-lighter">Todos</h1>
 			<input type="text" placeholder=" Introduce tu texto aqui" onKeyDown={
 				(e) => {
@@ -25,8 +17,9 @@ const Home = () => {
 					setHistory([...history,e.target.value])}
 				}
 			}/>
-			<ul className="list-group">
-				{ history.map( h => {return<li className="list-group-item" > {h}<span><i className="fa fa-trash"onClick={deleteItem}></i></span></li> })}
+			<ul className="list-group ">
+				{ history.map( h => {return<li className="list-group-item" > {h}<button type="button" class="btn-close" aria-label="Close"onClick={
+					(e) =>{e.target.parentElement.style.display ="none"}}></button></li> })}
 				
 			</ul>
 
